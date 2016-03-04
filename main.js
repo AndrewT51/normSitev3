531d20d7a0bfbe449595387b968784f454356795
Andrew Trigg <andrewt51@hotmail.com>
2016-03-04 12:20:36 +0000

    New layout works

--- main.js
+++ main.js
@@ -31,41 +31,41 @@ app.config(["$urlRouterProvider", "$stateProvider", "$locationProvider" ,functio
 }])
 
 app.controller("mainCtrl", ["$scope","$state", function($scope,$state){
-  var menuUp = true;
-  var toClearUnderline = [].slice.call(document.querySelectorAll("[data-index]"))
-  toClearUnderline.forEach(function(el){
-    el.setAttribute("id","")
-  })
-  var dropdown = document.getElementsByClassName("mobile-view")[0];
-  var menuButton = document.getElementsByClassName("nav-btn")[0];
+  var menuDown = false;
+  var clearSelected = document.querySelector(".selected");
+  if(clearSelected){
+    clearSelected.setAttribute('class','');
+  }
+  var dropdown = document.getElementById("main-navbar");
+  var menuButton = document.getElementsByClassName("drop-btn")[0];
   $scope.menuItems = data.sections;
 
   // Check what menu item this is in the array, it will correlate with the data-num on the page menu 
-  // var pageHighlight;
-  // data.sections.forEach(function(obj,index){
-  //   if(obj.url === $state.$current.name){
-  //     pageHighlight = index
-  //   }
-  // })
+  var pageHighlight;
+  data.sections.forEach(function(obj,index){
+    if(obj.url === $state.$current.name){
+      pageHighlight = index
+    }
+  })
 
-  // var underline = document.querySelector("[data-index='" + pageHighlight + "']") || null;
-  // if(underline){
-  //   underline.setAttribute("id","bar-selected");
-  // } 
+  var underline = document.querySelector("[data-item='" + pageHighlight + "']") || null;
+  if(underline){
+    underline.setAttribute("class","selected");
+    // underline.className += "selected ";
+  } 
   
-  // function handleDropdown(){
-  //   menuUp = !menuUp;
-  //   dropdown.style.display = menuUp ?  "none" : "block";
-  //   menuUp ?  menuButton.className = menuButton.className.replace(/ btn-selected/,"") : menuButton.className += " btn-selected";
-  // }
+  function handleDropdown(){
+    menuDown = !menuDown;
+    menuDown ?  dropdown.className = dropdown.className+="down " : dropdown.className ="";
+  }
 
-  // $scope.dropMenu = function(){
-  //   handleDropdown();
-  // };
+  $scope.dropMenu = function(){
+    handleDropdown();
+  };
 
-  // $scope.menuClick = function(item, index){
-  //   handleDropdown();
-  //   $state.go(item.url)
-  // }
+  $scope.menuClick = function(item, index){
+    handleDropdown();
+    $state.go(item.url)
+  }
 
 }])
